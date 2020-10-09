4a0a5415caeef0c2d5c9b3e9258295fb2d993553
itizidon <dng21@binghamton.edu>
2020-10-09 18:33:47 -0400

    changed mongoDB to local

--- server.go
+++ server.go
@@ -9,36 +9,36 @@ import (
 	"io"
 
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-go-driver/bson/primitives"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 )
 
-func main() {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://itizidon:testingThisDb1@cluster0.nttrq.mongodb.net/<dbname>?retryWrites=true&w=majority"))
-	if err != nil {
-		log.Fatal(err)
-	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer client.Disconnect(ctx)
-	err = client.Ping(ctx, readpref.Primary())
-	if err != nil {
-		log.Fatal(err)
-	}
-	databases, err := client.ListDatabaseNames(ctx, bson.M{})
-	if err != nil {
-		log.Fatal(err)
-	}
+type User struct {
+	ID primitive.ObjectID 'json:"_id,omitempty" bson:"_id,omitempty"'
+	Firstname string 'json:"firstname,omitempty" bson:"firstname,omitempty"'
+	Lastname string 'json:"lastname,omitempty" bson:"lastname,omitempty"'
 
-	http.HandleFunc("/", homeHandler)
-	fmt.Println(databases)
+}
+
+var client *mongo.Client
+
+func main() {
+	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+	client, _ = mongo.Connect(ctx, "mongodb://lobalhost:27017")
+	http.HandleFunc("/person", homeHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func homeHandler(w http.ResponseWriter, r *http.Request){
-	io.WriteString(w, "w")
+	fmt.Println('reached')
+	switch r.Method{
+	case "POST":
+		var person Person = json.NewDecoder(r.Body).Decode(&person)
+		collection := client.Database("chef").Collection("people")
+		ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
+		result, _ := collection.InsertOne(ctx, person)
+		json.NewEncoder(r).Encode(result)
+	}
 }
